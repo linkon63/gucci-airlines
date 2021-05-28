87718549927e357168cf7c406a602894db2ce24f
Rakib544 <md.rakib10122003@gmail.com>
2021-05-28 23:20:11 +0600

    booking page simple design done

---
+++ src/components/Main/CheckoutPage/CheckoutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CheckoutPage = () => {
+    return (
+        <section>
+            This is checkout page
+        </section>
+    );
+};
+
+export default CheckoutPage;
