3dbaa75b17195e96f169dac6733cb009bf27605c
Rakib544 <md.rakib10122003@gmail.com>
2021-05-29 23:18:27 +0600

    login set complate

--- src/App.js
+++ src/App.js
@@ -1,45 +1,34 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Booking from './components/Main/Booking.js/Booking';
 import CheckoutPage from './components/Main/CheckoutPage/CheckoutPage';
 import Login from './components/Main/Login/Login';
 import NotFound from './components/Main/NotFound/NotFound';
-import { PrivateRoute, useProvideAuth } from './lib/auth';
-import { addUserInfo } from './redux/actions/airLinesBookingAction';
+import { PrivateRoute } from './lib/auth';
 
 function App() {
-  const auth = useProvideAuth();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(addUserInfo(auth))
-  }, [])
 
   return (
-    // <AuthProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <PrivateRoute path="/book/:id">
-            <Booking />
-          </PrivateRoute>
-          <PrivateRoute path="/checkout">
-            <CheckoutPage />
-          </PrivateRoute>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    // </AuthProvider>
-
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <PrivateRoute path="/book/:id">
+          <Booking />
+        </PrivateRoute>
+        <PrivateRoute path="/checkout">
+          <CheckoutPage />
+        </PrivateRoute>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,29 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Redirect, Route } from "react-router";
 import firebase from "./firebase";
-import Preloader from '../components/Shared/Preloader';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addUserInfo } from "../redux/actions/airLinesBookingAction";
 
 export const useAuth = () => {
     const authValue = useSelector(state => state.airlinesReducers.userInfo);
     return authValue
 };
 
-export const PrivateRoute = ({
-    children,
-    ...rest
-}) => {
-    const logInData = useAuth();
-    console.log(logInData[0])
-    const data = localStorage.getItem('user')
-    console.log(data)
+export const PrivateRoute = ({children, ...rest}) => {
+    const [user, setUser] = useState();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                const formattedUser = formatUser(user);
+                setUser(formattedUser)
+                dispatch(addUserInfo(formattedUser))
+            }
+        });
+
+        return () => unsubscribe();
+    }, [dispatch]);
+
+    const userInfo = useSelector(state => state.airlinesReducers.userInfo[0]);
+
     return (
         <Route
             {...rest}
             render={({ location }) =>
-            logInData[0].loginStatus.status === "pending" ? (
-                    <Preloader />
-                ) : logInData[0].user || localStorage.getItem('user') ? (
+            userInfo?.email || user?.email ? (
                     children
                 ) : (
                     <Redirect
@@ -38,69 +46,32 @@ export const PrivateRoute = ({
     );
 };
 
-export function useProvideAuth() {
-    const [user, setUser] = useState();
-    const [loginStatus, setLoginStatus] = useState({
-        status: "idle",
-        error: null,
-    });
+const formatUser = (user) => ({
+    email: user.email,
+    name: user.displayName,
+    provider: user.providerData[0].providerId,
+    photoUrl: user.photoURL,
+    uid: user.uid,
+});
 
-    const formatUser = (user) => ({
-        email: user.email,
-        name: user.displayName,
-        provider: user.providerData[0].providerId,
-        photoUrl: user.photoURL,
-        uid: user.uid,
-    });
-
-    const signInWithGoogle = () => {
-        return firebase
-            .auth()
-            .signInWithPopup(new firebase.auth.GoogleAuthProvider())
-            .then((response) => {
-                const formattedUser = formatUser(response.user);
-                localStorage.setItem('user', response.user.email)
-                setUser(formattedUser);
-                setLoginStatus({ status: "resolved", error: null });
-                return formattedUser;
-            })
-            .catch((err) => {
-                setUser(null);
-                setLoginStatus({ status: "resolved", error: err.message });
-            });
-    };
-
-    const signOut = () => {
-        return firebase
-            .auth()
-            .signOut()
-            .then(() => {
-                localStorage.setItem('user', {});
-                setUser(null);
-                setLoginStatus({ status: "idle", error: null });
-            });
-    };
-
-    useEffect(() => {
-        setLoginStatus({ status: "pending", error: null });
-        const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                const formattedUser = formatUser(user);
-                setUser(formattedUser);
-                setLoginStatus({ status: "resolved", error: null });
-            } else {
-                setLoginStatus({ status: "idle", error: null });
-                setUser(false);
-            }
+export const signInWithGoogle = () => {
+    return firebase
+        .auth()
+        .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+        .then((response) => {
+            const formattedUser = formatUser(response.user);
+            return formattedUser;
+        })
+        .catch((err) => {
+            console.log(err)
         });
+};
 
-        return () => unsubscribe();
-    }, []);
-
-    return {
-        user,
-        loginStatus,
-        signInWithGoogle,
-        signOut,
-    };
-}
+export const signOut = () => {
+    return firebase
+        .auth()
+        .signOut()
+        .then(() => {
+            console.log('logout complete')
+        });
+};
