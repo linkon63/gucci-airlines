52f6ee64d0139b9e85488bc05a28a60e6df934c5
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-05-30 01:39:35 +0600

    RABIUL

--- src/components/Main/CheckoutPage/CheckoutPage.js
+++ src/components/Main/CheckoutPage/CheckoutPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { PaymentCart } from './PaymentCart';
+import Stripe from '../../../images/stripe.svg';
 
 const stripePromise = loadStripe('pk_test_51Ig0UgJGVGYGB4Cnu7o1bsmCeLLLnRCJL85Gkanxfmk6DFk91y6a3jP3E6eh9mI5bdYCtVQ3vuYMDaznhqDaZKn200jcUC5R6K');
 
@@ -9,7 +10,10 @@ const CheckoutPage = () => {
     return (
         <div className="container">
             <div className="row mt-5 py-5">
-                <div className="col-12 col-sm-12 col-md-9 col-lg-6 mx-auto bg-light shadow-sm p-5">
+                <div className="col-md-6">
+                <img src={Stripe} alt="" srcset="" className="img-fluid"/>
+                </div>
+                <div className="col-md-6 mx-auto shadow-sm p-5">
                     <Elements stripe={stripePromise}>
                         <PaymentCart />
                     </Elements>
