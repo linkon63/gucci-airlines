efad888b7c5e02b6ecf19f3875f2de0baa3f5bbc
Rakib544 <md.rakib10122003@gmail.com>
2021-05-29 13:34:34 +0600

    redux add to booking list functionality work done

--- src/components/Main/CheckoutPage/PaymentCart.js
+++ src/components/Main/CheckoutPage/PaymentCart.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToBookingList } from '../../../redux/actions/airLinesBookingAction';
 
 export const PaymentCart = ({ orderDetails }) => {
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null)
   const stripe = useStripe();
   const elements = useElements();
+
+  const dispatch = useDispatch();
+  const temporaryBookingData = useSelector(state => state.airlinesReducers.temporaryBookingList[0])
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -27,9 +32,12 @@ export const PaymentCart = ({ orderDetails }) => {
       setError(null)
       const paymentId = paymentMethod.id;
       const paymentWith = paymentMethod.card.brand;
-        
+      const bookingData = {...temporaryBookingData, paymentId, paymentWith}
+      dispatch(addToBookingList(bookingData))
     }
   };
+
+  // useSelector(state => console.log(state))
 
   return (
     <form onSubmit={handleSubmit}>
@@ -37,8 +45,8 @@ export const PaymentCart = ({ orderDetails }) => {
       <button className="btn btn-dark mt-3" type="submit" disabled={!stripe}>
         Pay
       </button>
-      <p className="success">{success}</p>
-      <p className="error">{error}</p>
+      <p className="pt-4 text-primary text-center">{success}</p>
+      <p className="pt-4 text-danger text-center">{error}</p>
     </form>
   );
 };
