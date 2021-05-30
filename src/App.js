48e6e01c244d234183bea0d23a38af1ea5639f80
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-05-30 15:55:58 +0600

    blog

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/Main/About/About.js';
+import Blog from './components/Main/Blog/Blog.js';
 import Booking from './components/Main/Booking.js/Booking';
 import CheckoutPage from './components/Main/CheckoutPage/CheckoutPage';
 import Login from './components/Main/Login/Login';
@@ -22,6 +23,9 @@ function App() {
         </Route>
         <Route path="/about">
           <About />
+        </Route>
+        <Route path="/blog">
+          <Blog />
         </Route>
         <PrivateRoute path="/book/:id">
           <Booking />
