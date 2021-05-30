6bbc680ffd06efae9d7eafa847be53ada47b6f54
Rakib544 <md.rakib10122003@gmail.com>
2021-05-30 17:47:59 +0600

    some set up changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/Main/About/About.js';
@@ -8,9 +9,24 @@ import CheckoutPage from './components/Main/CheckoutPage/CheckoutPage';
 import Login from './components/Main/Login/Login';
 import NotFound from './components/Main/NotFound/NotFound';
 import ThankYouPage from './components/Main/Thankyou/ThankyouPage';
-import { PrivateRoute } from './lib/auth';
+import { formatUser, PrivateRoute } from './lib/auth';
+import { addUserInfo } from './redux/actions/airLinesBookingAction';
+import firebase from "../src/lib/firebase";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            const formattedUser = formatUser(user);
+            console.log(formattedUser)
+            dispatch(addUserInfo(formattedUser))
+        }
+    });
+
+    return () => unsubscribe();
+}, []);
 
   return (
     <BrowserRouter>
